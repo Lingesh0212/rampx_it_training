b6106910c13850509af010ce198650392e3506e1
lingesh <lingeshlingesh881@gmail.com>
2026-07-20 13:34:16 +0530

    java program

--- day_19/classtask/MaxUsingHashMap.java
+++ day_19/classtask/MaxUsingHashMap.java
@@ -1,16 +1,16 @@
-package day_19.classtask;
+                                                                  package day_19.classtask;
 
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class MaxUsingHashMap {
+public class zMaxUsingHashMap {
 
         public static void main(String [] args){
             HashMap<String ,Integer> hm = new HashMap<String ,Integer>();
-            hm.put("Sibi",90);
-            hm.put("thara",80);
+            hm.put("Sib",9);
+                    hm.put("thara",80);
             hm.put("Anu",75);
             hm.put("Vasu",85);
             int max = 0;
@@ -23,7 +23,6 @@ public class MaxUsingHashMap {
             }
             System.out.println(max);
             System.out.println(name);
-        }
     }
 
 
